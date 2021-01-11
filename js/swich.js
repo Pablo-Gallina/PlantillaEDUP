85dfcd775af4d937c054d201f918ec146375565b
Pablo <pdavidggil@gmail.com>
2021-01-10 20:05:49 -0600

    Version 1.0

---
+++ js/swich.js
@@ -0,0 +1,11 @@
+//$('input.cb-value').prop("checked", true);
+$('.cb-value').click(function () {
+  var mainParent = $(this).parent('.toggle-btn');
+  if ($(mainParent).find('input.cb-value').is(':checked')) {
+    $(mainParent).addClass('active');
+  } else {
+    $(mainParent).removeClass('active');
+  }
+
+});
+//# sourceURL=pen.js
